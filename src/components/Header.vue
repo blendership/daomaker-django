1bdaab31abe4ca8752df4af7416ad67d6a11f596
blendership <116368313+blendership@users.noreply.github.com>
2020-05-12 17:57:21 +0000

    Wip

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,4 +2,31 @@
   <v-layout
     row
     wrap
-    j
+    justify-center
+  >
+    <v-flex sm2 md3 xs12 align-self-center order-xs2 order-sm1 order-md1>
+      <v-container justify-center class="no-padding-xs" v-if="$route.name === 'shader'">
+        <v-btn
+          to="/"
+          color="info"
+          block
+        >
+          <v-icon
+            dark
+          >
+            apps
+          </v-icon>
+        </v-btn>
+      </v-container>
+    </v-flex>
+    <v-flex sm8 md6 xs12 order-xs1 order-sm2 order-md2>
+      <v-container text-xs-center text-md-center
+      >
+        <v-btn
+          :to="$route.name === 'about' ? '/' : '/about'"
+          class="app-name"
+          flat
+        >
+          <img
+            alt="logo"
+            src="@/asse
