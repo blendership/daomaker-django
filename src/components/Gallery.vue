6a7ab80067f2603f3071cd19897109dbb5a98764
blendership <116368313+blendership@users.noreply.github.com>
2020-05-12 05:51:50 +0000

    delete line

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -2,3 +2,35 @@
   <v-layout
     class="gallery-container"
     justify-center
+  >
+    <v-flex
+      xs12
+    >
+      <v-card>
+        <v-container
+          fluid
+          grid-list-md
+        >
+          <v-layout
+            row
+            wrap
+          >
+            <v-flex
+              v-for="visualization in visualizationsArray"
+              :key="visualization.id"
+              xs6 sm4 md3
+            >
+              <v-card
+                :to="`/shader/${visualization.id}`"
+                hover
+              >
+                <v-img
+                  :src="getShaderScreenshots(visualization.id)"
+                >
+                  <v-container
+                    fill-height
+                    fluid
+                    pa-2
+                  >
+                    <v-layout fill-height>
+           
