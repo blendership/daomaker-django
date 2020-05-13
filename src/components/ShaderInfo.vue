02387f83705090748aaf2588b5adb00b11852041
blendership <116368313+blendership@users.noreply.github.com>
2020-05-13 10:03:19 +0000

    drivers: soc: Add buffer overflow check for svc send request

--- src/components/ShaderInfo.vue
+++ src/components/ShaderInfo.vue
@@ -1,2 +1,32 @@
 <template>
- 
+  <v-container
+    class="shader-info"
+    justify-center
+  >
+    <v-flex
+      xs12
+      md6
+      offset-md3
+    >
+      <v-card color="purple" class="white--text">
+        <v-layout row>
+          <v-flex xs12>
+            <v-card-title primary-title>
+              <div>
+                <div class="headline">{{selectedVisualization.name}}</div>
+              </div>
+            </v-card-title>
+          </v-flex>
+        </v-layout>
+        <v-divider light></v-divider>
+        <v-card-actions class="pa-3">
+          <v-btn
+            :href="selectedVisualization.original"
+            target="_blank"
+          >
+            <v-icon
+              left
+              dark
+            >
+              open_in_new
+        
