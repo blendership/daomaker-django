f073cd321e34903e70886104fc811b780db058d0
blendership <116368313+blendership@users.noreply.github.com>
2020-05-14 11:16:52 +0000

    logs removed

--- src/modules/ShaderEngine/glslWrapper.js
+++ src/modules/ShaderEngine/glslWrapper.js
@@ -20,4 +20,41 @@ class GlslWrapper {
 
     fragmentShader += `
       
-// fragme
+// fragment code
+
+${this.shaderEngine.shader.fragmentShader}
+
+`;
+    if(!this.shaderEngine.shader.fragmentShader.includes('main(')) {
+
+      if(!this.shaderEngine.shader.fragmentShader.includes('mainVR(')) {
+
+        fragmentShader += `
+      
+void mainVR( out vec4 fragColor, in vec2 fragCoord, in vec3 fragRayOri, in vec3 fragRayDir )
+{
+
+  mainImage(fragColor, fragCoord);
+}
+
+`;
+      }
+
+      if(renderType === 'image') {
+        fragmentShader += `
+      
+// main image function
+
+${ImageWrapper}
+
+`;
+      }
+      if(renderType === 'vr') {
+        fragmentShader += `
+
+// main VR function
+
+#define VR_SETTINGS_CARDBOARD
+//#define VR_SETTINGS_USE_MOUSE
+#define VR_SETTINGS_FIXED_ROTATION
+//#define VR_SETTINGS_DEVICE_ORIE
