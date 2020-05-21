eff46985c139f6cdfc07cac99beebd73cf5d7e79
blendership <116368313+blendership@users.noreply.github.com>
2020-05-21 13:31:25 +0000

    added new class

--- src/shaders/shader4.js
+++ src/shaders/shader4.js
@@ -5,4 +5,17 @@ export default {
   params: [
     {
       name: 'control1',
-      t
+      type: 'f',
+      range: [0, 5],
+      defaultValue: 2.5,
+      step: 0.01
+    },
+    {
+      name: 'control2',
+      type: 'f',
+      range: [0.5, 0.7],
+      defaultValue: 0.66,
+      step: 0.001
+    }
+  ],
+}
---
+++ src/shaders/shader5.js
@@ -0,0 +1,7 @@
+
+export default {
+  original: 'https://www.shadertoy.com/view/3dlSz2',
+  name: 'Grid Landscape',
+  fragmentShader: require('./shader5.glsl'),
+  //params: [],
+}
