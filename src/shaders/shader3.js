3c9ab813f2ea4aa0d2529093e9c96f913f5bbee9
blendership <116368313+blendership@users.noreply.github.com>
2020-05-21 12:33:59 +0000

    reorganized tests

--- src/shaders/shader3.js
+++ src/shaders/shader3.js
@@ -1,2 +1,28 @@
 export default {
-  o
+  original: 'http://glslsandbox.com/e#53200.0',
+  name: 'Blobs',
+  fragmentShader: require('./shader3.glsl'),
+  params: [
+    {
+      name: 'sizeUniform',
+      type: 'f',
+      range: [0., 1.],
+      defaultValue: 0.4,
+      step: 0.01,
+    },
+    {
+      name: 'colorUniform',
+      type: 'f',
+      range: [0.1, 0.9],
+      defaultValue: 0.6,
+      step: 0.01,
+    },
+    {
+      name: 'countUniform',
+      type: 'f',
+      range: [1., 50.],
+      defaultValue: 15.,
+      step: 0.3,
+    },
+  ],
+}
---
+++ src/shaders/shader4.js
@@ -0,0 +1,8 @@
+export default {
+  original: 'https://www.shadertoy.com/view/wdlSD2',
+  name: 'Anaglyph fractal',
+  fragmentShader: require('./shader4.glsl'),
+  params: [
+    {
+      name: 'control1',
+      t
