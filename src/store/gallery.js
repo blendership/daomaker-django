c9369584808b32a9b298d9d6cf5f2db4c819a615
blendership <116368313+blendership@users.noreply.github.com>
2020-05-23 07:37:23 +0000

    Updated chef versions

--- src/store/gallery.js
+++ src/store/gallery.js
@@ -11,3 +11,41 @@ const GalleryModule = {
 
   mutations: {
 
+    setVisualisations: (state, visualizations) => {
+
+      state.visualizations = visualizations;
+
+    },
+
+    setVisualisationId(state, visualisationId) {
+
+      state.selectedVisualizationId = visualisationId;
+
+    },
+
+  },
+
+  actions: {
+
+    loadVisualisations: ({ state, commit }) => {
+
+      if(Object.keys(state.visualizations).length === 0){
+
+        commit('setVisualisations',
+          Object.keys(shaders).reduce((acc, shaderId) =>
+              ({
+                ...acc,
+                [shaderId]: {
+                  ...shaders[shaderId],
+                  id: shaderId,
+                }
+              })
+            , {}
+          ));
+      }
+
+    },
+
+    setVisualisation({ state, commit }, visualisationId) {
+
+      cons
