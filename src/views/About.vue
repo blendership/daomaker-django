85faa4796823b330d566595c05c439de94387f75
blendership <116368313+blendership@users.noreply.github.com>
2020-05-23 16:12:47 +0000

    Update upload.sh

--- src/views/About.vue
+++ src/views/About.vue
@@ -7,4 +7,26 @@
     <v-card>
       <v-container>
         <v-layout justify-center>
-     
+          <img
+            alt="logo"
+            src="@/assets/logo-192.png"
+          >
+        </v-layout>
+        <h2 class="headline">
+          Description
+        </h2>
+
+        <v-card-text>
+          <b>Acidoscope</b> is a web app that allows to display interactive visualisations.
+        </v-card-text>
+
+        <h2 class="headline">
+          Keyboard shortcuts
+        </h2>
+        <v-card-text>
+          <p>Some actions can be triggered by pressing the keyboard:</p>
+
+          <v-layout row wrap>
+            <v-flex xs12 sm6><code>Space</code> Play/Pause animation</v-flex>
+            <v-flex xs12 sm6><code>C</code> Show/Hide controls in fullscreen</v-flex>
+            <v-
