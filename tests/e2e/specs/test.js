b4f903d74b0d4fae2393e1fe3915ad9ef2c6260b
blendership <116368313+blendership@users.noreply.github.com>
2020-05-25 14:42:43 +0000

    test test

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -19,4 +19,21 @@ describe('Test', () => {
     cy.get('#nav > a:nth-child(2)').click();
     cy.get('#shader-container').should('be.visible');
 
-    cy.get('#nav > a
+    cy.get('#nav > a:nth-child(1)').click();
+    cy.get('#gallery').should('be.visible');
+
+  });
+
+  describe('Shader', () => {
+
+    it('Should render shader', () => {
+
+      cy.visit('/shader');
+      cy.get('#shader-container').should('be.visible');
+      cy.get('#shader-container').find('canvas').should('be.visible');
+
+    });
+
+  });
+
+});
