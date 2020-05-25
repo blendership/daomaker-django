e222e1e3c2e6ade16256777b83cf957be8346855
blendership <116368313+blendership@users.noreply.github.com>
2020-05-25 22:04:24 +0000

    Try fix build

--- tests/unit/components/MidiLoader.spec.js
+++ tests/unit/components/MidiLoader.spec.js
@@ -11,3 +11,26 @@ describe('Components: Loaders', function (){
 
     this.sandbox = createSandbox();
     this.localVue = createLocalVue();
+    this.localVue.use(Vuex);
+
+    this.stubs = {
+      getters: {
+        midiHardwareConnected: this.sandbox.stub(),
+      },
+      actions: {
+        setMidiHardwareStatus: this.sandbox.stub(),
+      }
+    };
+    this.store = new Vuex.Store({
+      getters: {
+        midiHardwareConnected: this.stubs.getters.midiHardwareConnected,
+      },
+      actions: {
+        setMidiHardwareStatus: this.stubs.actions.setMidiHardwareStatus,
+      }
+    });
+
+    this.shallowConfig = { store: this.store,  localVue: this.localVue};
+    this.stubMidiIsSupported = this.sandbox.stub(Midi, 'isSupported').returns(true);
+    this.stubMidiRequestAccess = this.sandbox.stub(Midi, 'requestAccess').resolves();
+    this.
