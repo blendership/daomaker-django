bf98a34831e06047aa53c5f254d84c042ff06de5
blendership <116368313+blendership@users.noreply.github.com>
2020-05-27 10:13:17 +0000

    ahci: Add AMD CZ SATA device ID

--- tests/unit/components/ShaderRender.spec.js
+++ tests/unit/components/ShaderRender.spec.js
@@ -20,4 +20,34 @@ describe('Components: ShaderRender', function (){
     this.store = new Vuex.Store({
       actions: {
         createShaderEngine: this.stubs.actions.createShaderEngine,
-        stopShaderEngine: this.stubs.actions.stopShader
+        stopShaderEngine: this.stubs.actions.stopShaderEngine,
+      }
+    });
+
+    this.shallowConfig = { store: this.store,  localVue: this.localVue};
+
+  });
+
+  afterEach(() => {
+
+    this.sandbox.restore();
+
+  });
+
+  describe('Mount', () => {
+
+    it('Should create shader engine on mount', async () => {
+
+      const wrapper = shallowMount(ShaderRender, this.shallowConfig);
+
+      expect(this.stubs.actions.createShaderEngine.calledOnce).to.be.true;
+      expect(wrapper.contains('#shader-container') ).to.be.true;
+
+    });
+
+    it('Should stop shader engine on destroy', async () => {
+
+      const wrapper = shallowMount(ShaderRender, this.shallowConfig);
+
+      wrapper.destroy();
+      expect(this.stubs.actions.stopShaderEngine.called
